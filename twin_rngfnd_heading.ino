7a16af128d1c605f7252dd7c8570a09cc4ebc2d0
chobits <chobits@itri.org.tw>
2019-10-15 09:59:42 +0800

    add left wall config

--- twin_rngfnd_heading.ino
+++ twin_rngfnd_heading.ino
@@ -2,6 +2,8 @@
 #include <VL53L1X.h>
 #include "common/mavlink.h"
 
+//#define RIGHT_WALL
+
 #define UWB_TAG_FRAME_NOT_FOUND 0
 #define UWB_TAG_FRAME_HEADER 1
 #define UWB_TAG_FRAME_FOUND 2
@@ -10,8 +12,10 @@
 
 #define DIST_TWIN_RNGFND_MM 300.0
 
-#define SHUT_GPIO_F 19
-#define SHUT_GPIO_R 18
+#define SHUT_GPIO_RF 19
+#define SHUT_GPIO_RR 18
+#define SHUT_GPIO_LF 20
+#define SHUT_GPIO_LR 21
 
 VL53L1X sensor_r;
 VL53L1X sensor_f;
@@ -27,41 +31,74 @@ float yaw = 0;
 unsigned long prv_ts = 0;
 
 void setup()
-{
-  randomSeed(analogRead(0));
-  
-  delay(3000);
-  
-  Serial.begin(9600);
-  Serial1.setTimeout(10);
+{  
+  //randomSeed(analogRead(3));
+
+  pinMode(SHUT_GPIO_RF, OUTPUT);
+  pinMode(SHUT_GPIO_RR, OUTPUT);
+  pinMode(SHUT_GPIO_LF, OUTPUT);
+  pinMode(SHUT_GPIO_LR, OUTPUT);
+  digitalWrite(SHUT_GPIO_RF, LOW);
+  digitalWrite(SHUT_GPIO_RR, LOW);
+  digitalWrite(SHUT_GPIO_LF, LOW);
+  digitalWrite(SHUT_GPIO_LR, LOW);
+  delay(10);     
+
   Serial1.begin(115200);
+  Serial1.setTimeout(10);
   Wire.begin();
-  Wire.setClock(400000); // use 400 kHz I2C
+  Wire.setClock(400000); // use 400 kHz I2C  
 
-  pinMode(SHUT_GPIO_F, OUTPUT);
-  pinMode(SHUT_GPIO_R, OUTPUT);
-  digitalWrite(SHUT_GPIO_F, LOW);
-  digitalWrite(SHUT_GPIO_R, LOW);
-  delay(500);
-
-  digitalWrite(SHUT_GPIO_F, HIGH);
-  delay(500);
-  sensor_f.setTimeout(500);
-  if (!sensor_f.init())
+  Serial.println("init sensor_f...");
+#ifdef RIGHT_WALL
+  digitalWrite(SHUT_GPIO_RF, HIGH);
+#else
+  digitalWrite(SHUT_GPIO_LF, HIGH);
+#endif
+  delay(10);
+  sensor_f.setTimeout(500);  
+  while (!sensor_f.init())
   {
     Serial.println("Failed to detect and initialize sensor_f!");
-    while (1);
+#ifdef RIGHT_WALL    
+    digitalWrite(SHUT_GPIO_RF, LOW);
+    delay(10);
+    digitalWrite(SHUT_GPIO_RF, HIGH);
+    delay(10);
+#else
+    digitalWrite(SHUT_GPIO_LF, LOW);
+    delay(10);
+    digitalWrite(SHUT_GPIO_LF, HIGH);
+    delay(10);
+#endif    
   }
   sensor_f.setAddress(0x2b);
-
-  digitalWrite(SHUT_GPIO_R, HIGH);
-  delay(500);
+  Serial.println("sensor_f ok");
+  
+  Serial.println("init sensor_r...");
+#ifdef RIGHT_WALL
+  digitalWrite(SHUT_GPIO_RR, HIGH);
+#else
+  digitalWrite(SHUT_GPIO_LR, HIGH);  
+#endif  
+  delay(10);
   sensor_r.setTimeout(500);
-  if (!sensor_r.init())
+  while (!sensor_r.init())
   {
     Serial.println("Failed to detect and initialize sensor_r!");
-    while (1);
+#ifdef RIGHT_WALL
+    digitalWrite(SHUT_GPIO_RR, LOW);
+    delay(10);
+    digitalWrite(SHUT_GPIO_RR, HIGH);
+    delay(10);
+#else
+    digitalWrite(SHUT_GPIO_LR, LOW);
+    delay(10);
+    digitalWrite(SHUT_GPIO_LR, HIGH);
+    delay(10);
+#endif    
   }
+  Serial.println("sensor_r ok");
   
   // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
   // You can change these settings to adjust the performance of the sensor, but
@@ -79,6 +116,12 @@ void setup()
   // timing budget.
   sensor_r.startContinuous(90);
   sensor_f.startContinuous(90);
+  
+#ifdef RIGHT_WALL
+  Serial.println("right wall");
+#else
+  Serial.println("left wall");
+#endif
 }
 
 void loop()
@@ -131,6 +174,13 @@ void loop()
   //}  
   if (sensor_r.dataReady()) rr = sensor_r.read();
   if (sensor_f.dataReady()) rf = sensor_f.read();
+#if 0
+  if (rr > 0 || rf > 0) {
+    String msg = "rng:";
+    msg = msg + rr + ":" + rf;
+    Serial.println(msg);    
+  }  
+#endif  
   if (rr > 0 && rf > 0) {    
     double theta = 0;
     if (rf < rr) {
@@ -141,8 +191,11 @@ void loop()
       theta = atan2(rf - rr, DIST_TWIN_RNGFND_MM);
       yaw = PI * 0.5 - theta;
       dist_wall_m = (rf + rr) * 0.5 * cos(theta) * 0.001;
-    }        
-#if 0
+    }
+#ifndef RIGHT_WALL
+      dist_wall_m = -dist_wall_m;    
+#endif
+#if 1
     String msg = "rng:";
     msg = msg + rr + ":" + rf + ":" + theta + ":" + dist_wall_m;
     Serial.println(msg);
